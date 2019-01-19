53c022acc7d2d88399e195d687acf3bb74287414
Artenes Nogueira <artenesama@gmail.com>
2019-01-19 18:15:58 -0400

    Adds IME options to edit texts

--- app/src/main/java/io/github/artenes/familybudget/BankTransactionFragment.kt
+++ app/src/main/java/io/github/artenes/familybudget/BankTransactionFragment.kt
@@ -1,26 +1,29 @@
 package io.github.artenes.familybudget
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import io.github.artenes.familybudget.bankaccount.toCents
 import io.github.artenes.familybudget.data.BankTransaction
 
 import kotlinx.android.synthetic.main.transaction_view.view.*
 
-class BankTransactionFragment : Fragment(), View.OnClickListener {
+class BankTransactionFragment : Fragment(), View.OnClickListener, TextView.OnEditorActionListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.transaction_view, container, false)
 
         view.save.setOnClickListener(this)
+        view.description.setOnEditorActionListener(this)
 
         return view
     }
 
-    override fun onClick(v: View?) {
+    private fun save() {
         val value: String = view?.value?.text.toString()
         val description: String = view?.description?.text.toString()
 
@@ -31,6 +34,15 @@ class BankTransactionFragment : Fragment(), View.OnClickListener {
         val transaction = BankTransaction(value = value.toCents(), description =  description)
         Repository.INSTANCE.addTransactionToBradesco(transaction)
         activity?.finish()
+    }
+
+    override fun onClick(v: View?) {
+        save()
+    }
+
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+        save()
+        return true
     }
 
 }
